77f976d3315fbd049e39257742a86da3da739bc8
NaFis Fuad Mim <nafisnfm@gmail.com>
2022-04-03 13:35:41 +0600

    dphone

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-function updateCaseNumber(product, price, isIncrease) {
-  const caseInput = document.getElementById(product + "-number");
-  let caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncrease) {
+  const productInput = document.getElementById(product + "-number");
+  let productNumber = productInput.value;
   if (isIncrease) {
-    caseNumber = parseInt(caseNumber) + 1;
-  } else if (caseNumber > 0) {
-    caseNumber = parseInt(caseNumber) - 1;
+    productNumber = parseInt(productNumber) + 1;
+  } else if (productNumber > 0) {
+    productNumber = parseInt(productNumber) - 1;
   }
-  caseInput.value = caseNumber;
-  const caseTotal = document.getElementById(product + "-total");
-  caseTotal.innerText = caseNumber * price;
+  productInput.value = productNumber;
+  const productTotal = document.getElementById(product + "-total");
+  productTotal.innerText = productNumber * price;
 }
 //phone
 document.getElementById("phone-plus").addEventListener("click", function () {
-  updateCaseNumber("phone", 1219, true);
+  updateProductNumber("phone", 1219, true);
 });
 document.getElementById("phone-minus").addEventListener("click", function () {
-  updateCaseNumber("phone", 1219, false);
+  updateProductNumber("phone", 1219, false);
 });
 
 //case
 document.getElementById("case-plus").addEventListener("click", function () {
-  updateCaseNumber("case", 59, true);
+  updateProductNumber("case", 59, true);
 });
 document.getElementById("case-minus").addEventListener("click", function () {
-  updateCaseNumber("case", 59, false);
+  updateProductNumber("case", 59, false);
 });
