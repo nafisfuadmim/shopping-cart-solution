6a2da56ec81420d56770fc1a7183d7d36c970b25
NaFis Fuad Mim <nafisnfm@gmail.com>
2022-04-03 13:31:11 +0600

    phone

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-function updateCaseNumber(isIncrease) {
-  const inputField = document.getElementById("case-number");
-  const inputValue = inputField.value;
+function updateCaseNumber(product, price, isIncrease) {
+  const caseInput = document.getElementById(product + "-number");
+  let caseNumber = caseInput.value;
   if (isIncrease) {
-    inputField.value = parseInt(inputValue) + 1;
-  } else if (inputValue > 0) {
-    inputField.value = parseInt(inputValue) - 1;
+    caseNumber = parseInt(caseNumber) + 1;
+  } else if (caseNumber > 0) {
+    caseNumber = parseInt(caseNumber) - 1;
   }
+  caseInput.value = caseNumber;
+  const caseTotal = document.getElementById(product + "-total");
+  caseTotal.innerText = caseNumber * price;
 }
+//phone
+document.getElementById("phone-plus").addEventListener("click", function () {
+  updateCaseNumber("phone", 1219, true);
+});
+document.getElementById("phone-minus").addEventListener("click", function () {
+  updateCaseNumber("phone", 1219, false);
+});
+
+//case
 document.getElementById("case-plus").addEventListener("click", function () {
-  updateCaseNumber(true);
+  updateCaseNumber("case", 59, true);
 });
 document.getElementById("case-minus").addEventListener("click", function () {
-  updateCaseNumber(false);
+  updateCaseNumber("case", 59, false);
 });
