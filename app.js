de7b796433bbc69e28cba141b45ce1aaaa32c62c
NaFis Fuad Mim <nafisnfm@gmail.com>
2022-04-03 11:55:52 +0600

    fials

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("this is a book");
+function updateCaseNumber(isIncrease) {
+  const inputField = document.getElementById("case-number");
+  const inputValue = inputField.value;
+  if (isIncrease) {
+    inputField.value = parseInt(inputValue) + 1;
+  } else if (inputValue > 0) {
+    inputField.value = parseInt(inputValue) - 1;
+  }
+}
+document.getElementById("case-plus").addEventListener("click", function () {
+  updateCaseNumber(true);
+});
+document.getElementById("case-minus").addEventListener("click", function () {
+  updateCaseNumber(false);
+});
